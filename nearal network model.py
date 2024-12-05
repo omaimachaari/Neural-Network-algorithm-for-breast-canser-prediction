3f20f4991fc35f4b2f035630233788051e4efd84
omaimachaari <omaima.chaari202@gmail.com>
2024-12-05 16:01:16 +0100

    Add files via upload

---
+++ nearal network model.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+
+iris = load_breast_cancer()
+x, y = iris.data, iris.target
+
+one_hot_encoder = OneHotEncoder(sparse_output=False)
+y = one_hot_encoder.fit_transform(y.reshape(-1, 1))
+
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.2, random_state=42
+)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(10, input_shape=(x_train.shape[1],), activation="relu"),
+    tf.keras.layers.Dense(8, activation="relu"),
+    tf.keras.layers.Dense(2, activation="softmax")
+])
+
+model.compile(optimizer='adam', 
+              loss='categorical_crossentropy', 
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=50, batch_size=5, verbose=1)
+
+test_loss, test_accuracy = model.evaluate(x_test, y_test, verbose=0)
+print(f'Test Accuracy: {test_accuracy:.2f}')
